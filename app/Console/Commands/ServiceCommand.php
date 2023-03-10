bc902a46f7a55ddbeb4664ce1fd76d22bac473f5
Jarvis <tanmoydey8777@gmail.com>
2023-03-11 00:49:54 +0530

    update: service class stub

--- app/Console/Commands/ServiceCommand.php
+++ app/Console/Commands/ServiceCommand.php
@@ -3,7 +3,14 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use InvalidArgumentException;
 
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'make:service')]
 class ServiceCommand extends GeneratorCommand
 {
     /**
@@ -17,7 +24,7 @@ class ServiceCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $description = 'Create a Service Class.';
+    protected $description = 'Create a new service Class.';
 
     /**
      * Class type that is being created.
@@ -27,22 +34,112 @@ class ServiceCommand extends GeneratorCommand
      * exists, you'll receive a message
      * like this: $type already exists!
      */
-    protected $type = 'Service class';
+    protected $type = 'Service';
+
+    /**
+     * Build the class with the given name.
+     */
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        $model = $this->option('model');
+
+        if (!$model) {
+            throw new InvalidArgumentException('Please provide Model name with --model flag.');
+        }
+
+        return $this->replaceModel($stub, $model);
+    }
+
+    /**
+     * Replace the model for the given stub.
+     */
+    protected function replaceModel($stub, $model): string
+    {
+        $modelClass = $this->parseModel($model);
+
+        $replace = [
+            'DummyFullModelClass' => $modelClass,
+            '{{ namespacedModel }}' => $modelClass,
+            '{{namespacedModel}}' => $modelClass,
+            'DummyModelClass' => class_basename($modelClass),
+            '{{ model }}' => class_basename($modelClass),
+            '{{model}}' => class_basename($modelClass),
+            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
+            '{{ modelVariable }}' => lcfirst(class_basename($modelClass)),
+            '{{modelVariable}}' => lcfirst(class_basename($modelClass)),
+        ];
+
+        return str_replace(
+            array_keys($replace),
+            array_values($replace),
+            $stub
+        );
+    }
+
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string  $model
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        return $this->qualifyModel($model);
+    }
+
+    /**
+     * Get the console command arguments.
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the observer already exists'],
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'The model that the observer applies to'],
+        ];
+    }
 
     /**
      * Specify your Stub's location.
      */
-    protected function getStub()
+    protected function getStub(): string
     {
-        return  base_path().'/stubs/service.stub';
+        return  base_path() . '/stubs/service.stub';
     }
 
     /**
      * The root location where your new file should
      * be written to.
      */
-    protected function getDefaultNamespace($rootNamespace)
+    protected function getDefaultNamespace($rootNamespace): string
     {
-        return $rootNamespace.'\Services';
+        return $rootNamespace . '\Services';
+    }
+
+    /**
+     * Interact further with the user if they were prompted for missing arguments.
+     */
+    protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output): void
+    {
+        if ($this->isReservedName($this->getNameInput()) || $this->didReceiveOptions($input)) {
+            return;
+        }
+
+        $model = $this->components->askWithCompletion(
+            'What model should this service apply to?',
+            $this->possibleModels(),
+            'none'
+        );
+
+        if ($model && $model !== 'none') {
+            $input->setOption('model', $model);
+        }
     }
 }
